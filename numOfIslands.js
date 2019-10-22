2c766943bf3508c7cdeba7b254eabd65618ed36c
Marigrace Leeman <leemuly@gmail.com>
2019-10-22 18:43:26 -0400

    repo init

---
+++ numOfIslands.js
@@ -0,0 +1,15 @@
+// 1 represents land, 0 represents water
+// island is a block of land surrounded by water
+// horizontally or vertically adjacent
+//
+
+// ex: 6 islands
+// [
+// [0,1,0,1,0],
+// [0,0,1,1,1],
+// [1,0,0,1,0],
+// [0,1,1,0,0],
+// [1,0,1,0,1]
+// ]
+
+
