9bb498ae0bddb740544fcfd29fb5e76b5a26d336
Marigrace Leeman <leemuly@gmail.com>
2019-12-27 15:52:40 -0500

    unique pointers

---
+++ uniqueValues.js
@@ -0,0 +1,26 @@
+// Implement a function called uniqueValues which accepts a sorted array and counts that array's unique numbers. Negative numbers are possible, assume only sorted, valid values will be passed in.
+
+const uniqueValues = (arr) => {
+  if(arr.length === 0) return 0;
+  if(arr.length === 1) return 1;
+
+  let uniqueVals = 1;
+  let holder = 0;
+  let checker = 1;
+
+  while(checker < arr.length){
+    if(arr[checker] !== arr[holder]){
+      uniqueVals++;
+      holder = checker;
+    } else {
+      checker ++;
+    }
+  }
+
+  return uniqueVals;
+}
+
+console.log(uniqueValues([1, 1, 1, 1, 1, 2])); //2
+console.log(uniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); //7
+console.log(uniqueValues([])); //0
+console.log(uniqueValues([-2, -1, -1, 0, 1])); //4
